85edd8039ea4ca3a4e9d135e7844fe94e3d3e3e0
Augusto Moço Zambello <gutomz@outlook.com>
2022-05-06 12:31:18 -0300

    [SERVER] CORS
    Added cors to the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import { routes } from './routes';
 
 const port = 3333;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
