cdcf7dce3b4db1f18db1e4f13ad43020edf0fd3b
Augusto Moço Zambello <gutomz@outlook.com>
2022-05-06 12:22:48 -0300

    [SERVER] Unit Tests
    Added tests to create feedback usecase

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -18,6 +18,18 @@ export class SubmitFeedbackUseCase implements UseCase<SubmitFeedbackUseCaseReque
   async execute(param: SubmitFeedbackUseCaseRequest) {
     const { type, comment, screenshot } = param;
 
+    if (!type) {
+      throw new Error('Field [type] is required');
+    }
+
+    if (!comment) {
+      throw new Error('Field [comment] is required');
+    }
+
+    if (screenshot && !screenshot.startsWith('data:image/png;base64,')) {
+      throw new Error('Invalid screenshot format');
+    }
+
     await this.feedbacksRepository.create({
       type,
       comment,
