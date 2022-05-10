28991ce80cbdd1bfdad679965c3e924bab5e112b
Augusto Moço Zambello <gutomz@outlook.com>
2022-05-10 16:39:01 -0300

    [MOBILE] Widget Bottom Sheet

---
+++ mobile/src/utils/feedbackTypes.ts
@@ -0,0 +1,14 @@
+export const feedbackTypes = {
+  'BUG': {
+    title: 'Problema',
+    image: require('../assets/images/bug.png')
+  },
+  'IDEA': {
+    title: 'Ideia',
+    image: require('../assets/images/idea.png'),
+  },
+  'OTHER': {
+    title: 'Outro',
+    image: require('../assets/images/thought.png'),
+  },
+};
